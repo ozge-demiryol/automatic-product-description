7a05f43cd380f9663e64eb8d41fda465e0684b6b
Özge <demiryol.ozge@gmail.com>
2025-08-03 17:02:20 +0300

    chore: Remove redundant files

--- apps/backend/src/features/chat/chat.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { ChatController } from './chat.controller';
-
-const router = Router();
-const chatController = new ChatController();
-
-router.post('/', (req, res) => chatController.getChatResponse(req, res));
-
-export { router as chatRoutes };
--- apps/backend/src/features/faq/faq.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { FaqController } from './faq.controller';
-
-const router = Router({ mergeParams: true });
-const faqController = new FaqController();
-
-router.post('/', (req, res) => faqController.addFaq(req, res));
-export { router as faqRoutes };
