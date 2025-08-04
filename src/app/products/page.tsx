197ce851153907b7da1e54b1f2fbf9191c15f8d9
Özge <demiryol.ozge@gmail.com>
2025-08-04 18:01:38 +0300

    fix: Fix base url

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -14,7 +14,7 @@ interface Product {
 async function getProducts(): Promise<Product[]> {
   try {
     const apiBaseUrl = process.env.VERCEL_URL
-      ? process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
       : process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
     const res = await fetch(`${apiBaseUrl}/api/products`, {
       headers: {
